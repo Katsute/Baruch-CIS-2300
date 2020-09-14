8677a7f65522c3cda7e18b2bd1e762a02a62946e
Katsute <58778985+Katsute@users.noreply.github.com>
2020-09-14 15:52:40 -0400

    Create 09_14_2020.py

---
+++ classwork/09_14_2020.py
@@ -0,0 +1,40 @@
+# Q1
+
+x = float(input("First number: "))
+y = float(input("Second number: "))
+
+add = x + y
+sub = x - y
+
+print("Addition of two numbers is: ", add)
+
+print("Subtration of two number is: ", sub)
+
+# Q2
+
+weight = float(input("Weight (lbs): "))
+height = float(input("Height (in): "))
+
+bmi = 783 * weight / height ** 2
+print("BMI: ", bmi)
+
+# Q3
+
+# price = float(input("Price: $"))
+price = 20
+
+# discount = .90
+discount = float(input("Discount (%): "))
+
+sale_price = price * (1 - discount)
+
+
+print("Discount price: $", sale_price)
+
+# Boolean notes
+# > greater than
+# < less than
+# >= greater than or equal to
+# <= less than or equal to
+# == equal to
+# != not 
