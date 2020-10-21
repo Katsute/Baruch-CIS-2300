a1acd71167c9f85521230005cd0214536c924131
Katsute <58778985+Katsute@users.noreply.github.com>
2020-10-21 17:27:26 -0400

    Create homework_5.py

---
+++ assignments/homework_5.py
@@ -0,0 +1,47 @@
+def assertThat(expected, actual):
+    if expected != actual:
+        print(f"Assertion failed: Expected {expected} but got {actual}.")
+    return
+
+# 51177
+'''
+Given a variable n that contains a positive integer value, use two additional variables
+k and total to write a for loop to compute the sum of the cubes of the first n counting numbers,
+and store this value in total. Thus your code should put 1*1*1 + 2*2*2 + 3*3*3 + ... + n*n*n into total.
+Use no other variables other than n, k, and total.
+'''
+
+
+
+# 51259
+'''
+Compute the average of the numbers from 1 to n (where n is a positive integer value) 
+and assign it to the variable avg.
+'''
+
+assertThat(sum(range(n)) / n, avg)
+
+# 51250
+'''
+?
+'''
+
+# 51930
+'''
+?
+'''
+
+# 51934
+'''
+?
+'''
+
+# 51251
+'''
+?
+'''
+
+# 51253
+'''
+?
+'''
