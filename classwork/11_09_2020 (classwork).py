01f82623a23e0a7fd77e2eef6b0bd2e4a10f8b9f
Katsute <58778985+Katsute@users.noreply.github.com>
2020-11-10 13:03:55 -0500

    comments

--- classwork/11_09_2020 (classwork).py
+++ classwork/11_09_2020 (classwork).py
@@ -1,8 +1,11 @@
 def main():
+    # get
     sales = get_sales()
     advanced_pay = get_advanced_pay()
     rate = determined_comm_rate(sales)
+    # calc
     pay = sales * rate - advanced_pay
+    # print
     print("The pay is $", format(pay, ",.2f"), sep='')
     return
 
@@ -18,7 +21,7 @@ def determined_comm_rate(sales):
         return 0.14
     elif sales >= 18_000 and sales <= 21_999:
         return 0.16
-    else:
+    else:  # 22k+
         return 0.18
 
 def get_advanced_pay():
